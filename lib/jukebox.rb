f2db735c4c210e52f7e4af93998e25a60cf2eb4d
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-11-15 01:14:23 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -35,7 +35,8 @@ selection = gets.strip.to_s
 songs.each_with_index do |song, index|
   if selection == song 
     return_statement = "Playing #{song}"
-    elsif selection == 
+    elsif selection == selection == (index + 1).to_s
+    return_statement = 
   else
     return_statement = "Invalid input, please try again"
     break
