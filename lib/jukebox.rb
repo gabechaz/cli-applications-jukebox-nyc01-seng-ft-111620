25ffcbceefbc12ea2b977dee3f95dfeb4987e613
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-11-15 01:41:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -51,7 +51,7 @@ def play(songs)
     puts "Playing #{songs[selection.to_i - 1]}"
     elsif songs.include?(selection)
     puts "Playing #{selection}"
-  else 
+  else "Invalid input, please try again"
   end
 end
 def exit_jukebox
